9863db3993f16e7e7b212b862817beec29b5605e
Josel099 <joseltech2002@gmail.com>
2023-03-01 01:55:24 +0530

    the cards are called 2 . and the component RowPost  is reused

--- src/Components/Rowpost/RowPost.jsx
+++ src/Components/Rowpost/RowPost.jsx
@@ -2,7 +2,7 @@ import React,{useEffect,useState} from 'react'
 import "./RowPost.css"
 import axios from "../constants/axios"
 import { API_KEY,imageUrl } from '../constants/constant' 
-function RowPost() {
+function RowPost(props) {
   const [movies,setMovies] = useState([])
       useEffect(() => {
         axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response =>{
@@ -15,11 +15,11 @@ function RowPost() {
 
   return (
     <div className='row'>
-        <h2>Netflix orginals</h2>
+        <h2>{props.title}</h2>
         <div className='posters'>
 
           {movies.map((obj)=>{
-           <img className="poster" alt="poster" src={`${imageUrl.obj.backdrop_path}`}></img>
+           <img className={props.isSmall ? "smallPoster" : "poster" } alt="poster" src={`${imageUrl.obj.backdrop_path}`}></img>
           })}
      
      
