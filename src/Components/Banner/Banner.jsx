43894463214b72369bae765fc692dabc97ff8506
Josel099 <joseltech2002@gmail.com>
2023-02-28 22:57:04 +0530

    movie title and description are fetched from the api

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,22 +3,24 @@ import { useEffect } from 'react'
 import { API_KEY } from '../constants/constant'
 import axios from '../constants/axios'
 import "./Banner.css"
+import { useState } from 'react'
 function Banner() {
+  const [movie,setMovie] = useState()
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
-      
+      setMovie(response.data.results[0])
     })
   }, [])
-  
+
   return (
     <div className='banner'>
         <div className='content'>
-            <h1 className='title'> movie Name </h1>
-            <div>
+            <h1 className='title'> {movie ? movie.title: ""} </h1>
+            <div> 
                 <button className='button'>play</button>
                 <button className='button'>myList</button>
             </div>
-            <h1 className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nisi dolores itaque quam quas atque ipsum, doloremque debitis neque repellendus, voluptatibus veritatis molestias quae. Veritatis voluptate consequatur odit placeat corporis.</h1>
+            <h1 className='description'>{movie ? movie.overview : ""} </h1>
         </div>
         <div className="fade_bottom">
             
