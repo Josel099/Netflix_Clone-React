fcaa88be337aa6aa094d1e5a6932ea17f3664907
Josel Xavier <joseltech2002@gmail.com>
2023-04-07 15:20:45 +0530

    all links are added to url.jsx

--- src/Components/constants/urls.jsx
+++ src/Components/constants/urls.jsx
@@ -4,5 +4,5 @@ export const orginals = `discover/tv?api_key=${API_KEY}&with_networks=213`;
 export const action =`discover/movie?api_key=${API_KEY}&with_genres=28`;
 export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`;
 export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`;
-export const romance =`discover/movie?api_key=${API_KEY}&with_genres=10749`
-export const documentery=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_HERE}&with_genres=99`
+export const romance =`discover/movie?api_key=${API_KEY}&with_genres=10749`;
+export const documentery=`discover/movie?api_key=${API_KEY}&with_genres=99`
