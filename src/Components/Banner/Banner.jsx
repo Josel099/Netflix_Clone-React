2a699c526e536a97bb840d89db7a244dd09b1cb0
Josel Xavier <joseltech2002@gmail.com>
2023-04-07 18:34:14 +0530

    finel touch

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,44 +1,47 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { API_KEY ,imageUrl} from '../constants/constant'
-import axios from '../constants/axios'
-import "./Banner.css"
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { API_KEY, imageUrl } from "../constants/constant";
+import axios from "../constants/axios";
+import "./Banner.css";
+import { useState } from "react";
 function Banner() {
-  const [movie,setMovie] = useState()
+  const [movie, setMovie] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
-    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
-      setMovie(response.data.results[currentIndex])
-    })
-  }, [currentIndex])  ;
-
-  useEffect(()=>{
-    const interval =setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        return prevIndex === movie.length - 1 ? 0 : prevIndex +1;
+    axios
+      .get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        setMovie(response.data.results[currentIndex]);
       });
-    },5000)
+  }, [currentIndex]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        return movie && prevIndex === movie.length - 1 ? 0 : prevIndex + 1;
+      });
+    }, 5000);
     return () => clearInterval(interval);
-  },[movie]);
+  }, [movie]);
 
   return (
-    <div 
-    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
-    className='banner'>
-        <div className='content'>
-            <h1 className='title'> {movie ? movie.title: ""} </h1>
-            <div> 
-                <button className='button'>play</button>
-                <button className='button'>my List</button>
-            </div>
-            <h1 className='description'>{movie ? movie.overview : ""} </h1>
-        </div>  
-        <div className="fade_bottom">
-            
+    <div
+      style={{
+        backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})`,
+      }}
+      className="banner"
+    >
+      <div className="content">
+        <h1 className="title"> {movie ? movie.title : ""} </h1>
+        <div>
+          <button className="button">play</button>
+          <button className="button">my List</button>
         </div>
+        <h1 className="description">{movie ? movie.overview : ""} </h1>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- src/Components/Rowpost/RowPost.jsx
+++ src/Components/Rowpost/RowPost.jsx
@@ -1,58 +1,55 @@
-import React,{useEffect,useState} from 'react'
-import "./RowPost.css"
-import axios from "../constants/axios"
-import { imageUrl,API_KEY } from '../constants/constant' 
-import YouTube from 'react-youtube'
+import React, { useEffect, useState } from "react";
+import "./RowPost.css";
+import axios from "../constants/axios";
+import { imageUrl, API_KEY } from "../constants/constant";
+import YouTube from "react-youtube";
 function RowPost(props) {
-  const [urlId,setUrlId] = useState('');
-  const [movies,setMovies] = useState([])
-      useEffect(() => {
-        axios.get(props.url).then(response =>{
-          setMovies(response.data.results)
-        }) 
-       
-       
-      }, [])
-      const opts = {
-        height: '390',
-        width: '100%',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-        },
-      };
-      
-const handleMovie  = (id)=>{
-axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-if(response.data.results.length !==0){
-  setUrlId(response.data.results[0])
-} 
-else{
-  console.log("Array empty")
-}
-})
-}
+  const [urlId, setUrlId] = useState("");
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get(props.url).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("Array empty");
+        }
+      });
+  };
 
   return (
-    <div className='row'>
-        <h2>{props.title}</h2>
-     
-<div className='posters'>
-  {movies.map((obj) => (
-    <img
-      onClick={() => handleMovie(obj.id)}
-      className={props.isSmall ? 'smallPoster' : 'poster'}
-      alt='poster'
-      src={`${imageUrl}${obj.backdrop_path}`}
-    />
-  ))}
-</div>
+    <div className="row">
+      <h2>{props.title}</h2>
 
+      <div className="posters">
+        {movies.map((obj) => (
+          <img
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            alt="poster"
+            src={`${imageUrl}${obj.backdrop_path}`}
+          />
+        ))}
+      </div>
 
-
-     {urlId && <YouTube opts={opts} videoId={urlId.key}/> }
+      {urlId && <YouTube opts={opts} videoId={urlId.key} />}
     </div>
-  )
+  );
 }
 
-export default RowPost
+export default RowPost;
